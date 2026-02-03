c67773b0136dd9edc1fdb6354216bedd3a134604
gzuuus <gzuuus@protonmail.com>
2026-02-03 13:16:28 +0100

    build(chat): configure deployment base path and remove unused env

--- chat/vite.config.ts
+++ chat/vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/marmot-ts/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
